95b996d866cbe57e91bb96769d99671a7a401596
Abdoulk Diodoi <nes.zon36@gmail.com>
2026-02-16 14:31:41 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'app-a-platinum-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://www.chosic.com',
+  'https://assets.mixkit.co',
+  'https://assets.mixkit.co'
+];
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); });
+self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))); });
+
